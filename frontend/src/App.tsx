749410635982f64d03cc5d0ee747158150e2d54f
Ikrom Numonov <numanovikrom454@gmail.com>
2022-10-28 23:27:19 -0400

    Set up basic routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import Index from './routes/index';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Authentication from './routes/authentication/authentication';
+import Navigation from './routes/navigation/navigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route index element={<Index />}/>
+            <Route path="auth" element={<Authentication />} />
+          </ Route>
+        </Routes>
       </header>
     </div>
   );
---
+++ frontend/src/routes/navigation/navigation.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import "./navigation.css";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+
+    </Fragment>
+  )
+}
+
+export default Navigation;
