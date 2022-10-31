8fd8d81f96cd11cd292c81fd9bde2d39b2e83ac1
acfielder <afielder@ithaca.edu>
2022-10-31 13:16:58 -0400

    first route and base files

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,11 +1,9 @@
-#make route that returns a json dictionary of dummy data
-#get python interpreter
-
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
+
     folders = {
         "school" : ['desk', 'pencil', 'professor'],
         "names" : ['Ikrom', 'Arabella', 'Colleen', 'Numonov'],
