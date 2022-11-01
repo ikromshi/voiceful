e9d9b621f99a3514f06bb3e2314506a0bc4ab79f
Ikrom Numonov <numanovikrom454@gmail.com>
2022-11-01 11:45:11 -0400

    connected backend and frontend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+##################################
+# Cross-origin resource sharing (CORS) is a browser security feature that restricts
+# cross-origin HTTP requests that are initiated from scripts running in the browser. 
 
 from app import routes
