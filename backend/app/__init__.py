9fbeb44f17b6372dea05675479fdf5d14488f278
Ikrom Numonov <numanovikrom454@gmail.com>
2022-11-02 11:07:47 -0400

    backend updated

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
