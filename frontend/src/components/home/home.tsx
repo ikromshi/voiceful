3467c5478703ee2dbdf042c04fee9fdf4e9c1310
Ikrom Numonov <numanovikrom454@gmail.com>
2022-11-29 21:03:03 -0500

    fetching folders from backend/db and storing them in the app

--- frontend/src/components/home/home.tsx
+++ frontend/src/components/home/home.tsx
@@ -3,14 +3,20 @@ import axios from "axios";
 import { useContext, useEffect } from "react";
 import TextReader from "../text-reader/text-reader";
 import { UserContext } from "../../contexts/user.context";
+import { FolderContext } from "../../contexts/folder.context";
 
 const Home = () => {
   const { currentUser } = useContext(UserContext);
-  
+  const { folders, setFolders } = useContext(FolderContext);
+  const foldersAPI = "http://127.0.0.1:5000/folders";
+  const localFoldersPath = "/db/folders.json";
+
   const fetchFolders = async (url: string) => {
     axios({method: "GET", url: url, data: currentUser?.email})
       .then((response) => {
         console.log(response.data);
+        const { foldersJson } = response.data;
+        foldersJson && setFolders(foldersJson);
       }).catch((error) => {
         if (error.response) {
           console.log(error.response);
@@ -20,11 +26,7 @@ const Home = () => {
     });
   }
 
-  useEffect(() => {
-    const foldersAPI = "http://127.0.0.1:5000/folders";
-    const localFoldersPath = "/db/folders.json";
-    currentUser !== null ? fetchFolders(foldersAPI) : fetchFolders(localFoldersPath);
-  }, []);
+  currentUser !== null ? fetchFolders(foldersAPI) : fetchFolders(localFoldersPath);
 
   return (
     <TextReader />
