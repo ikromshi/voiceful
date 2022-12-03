4869b73bedfdbcaf536525fe3afdd68d8bc436c3
Ikrom Numonov <numanovikrom454@gmail.com>
2022-12-03 18:54:34 -0500

    payment form complete

--- frontend/src/components/payment-form/payment-form.tsx
+++ frontend/src/components/payment-form/payment-form.tsx
@@ -7,63 +7,60 @@ import "./payment-form.css";
 
 const PaymentForm = () => {
   const [success, setSuccess ] = useState(false)
-    const stripe = useStripe()
-    const elements = useElements()
+  const stripe = useStripe()
+  const elements = useElements()
 
     
 
-    const handleSubmit = async (e: FormEvent) => {
-      if (!stripe || !elements) {
-        console.log("errprrreonewknefe");
-        return;
-      }
-        e.preventDefault()
-        const {error, paymentMethod} = await stripe.createPaymentMethod({
-            type: "card",
-            card: elements.getElement(CardElement)!
-        })
+  const handleSubmit = async (e: FormEvent) => {
+    if (!stripe || !elements) {
+      console.log("errprrreonewknefe");
+      return;
+    }
+    e.preventDefault()
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement)!
+    })
 
 
     if(!error) {
-        try {
-            const {id} = paymentMethod
-            const response = await axios.post("http://localhost:4000/payment", {
-                amount: 1000,
-                id
-            })
+      try {
+        const {id} = paymentMethod;
+        const response = await axios.post("http://localhost:4000/payment", {
+          amount: 1000,
+          id
+        });
 
-            if(response.data.success) {
-                console.log("Successful payment")
-                setSuccess(true)
-            }
-
-        } catch (error) {
-            console.log("Error", error)
+        if(response.data.success) {
+          console.log("Successful payment")
+          setSuccess(true)
         }
+
+      } catch (error) {
+        console.log("Error", error);
+      }
     } else {
-        console.log(error.message)
+      console.log(error.message);
     }
-}
+  }
 
-    return (
-        <>
-        {!success ? 
-        <form onSubmit={handleSubmit}>
-            <fieldset className="FormGroup">
-                <div className="FormRow">
-                    <CardElement/>
-                </div>
-            </fieldset>
-            <button>Pay</button>
-        </form>
-        :
-       <div>
-           <h2>You just bought a sweet spatula congrats this is the best decision of you're life</h2>
-       </div> 
-        }
-            
-        </>
-    )
+  return (
+    <div className="payment-form-div">
+    {!success ? 
+      <form className="form-container" onSubmit={handleSubmit}>
+        <h1>Credit Card Payment: </h1>
+        <CardElement/>
+        <button className="payment-button">Pay</button>
+      </form>
+    :
+    <div className="payment-success">
+      <h1>Payment Successful.</h1>
+      <h2>Thanks for shopping with us!</h2>
+    </div> 
+    }
+    </div>
+  );
 }
 
 export default PaymentForm;
