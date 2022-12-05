7a47f3c0df4497b2e337a1c17d2b7fff5af5af22
Ikrom Numonov <numanovikrom454@gmail.com>
2022-12-05 10:51:22 -0500

    added lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,35 @@
-import Index from './routes/index';
 import { Route, Routes } from 'react-router-dom';
-import ProfileRoute from './routes/profile/profile.route';
-import Navigation from './routes/navigation/navigation';
-import SignIn from './routes/authentication/sign-in';
-import SignUp from './routes/authentication/sign-up';
-import Folders from './routes/folders/folders.route';
-import NewFolder from './components/new-folder/new-folder';
-import NewButton from './components/new-button/new-button';
-import Donation from './routes/donatation/donation';
+import Spinner from './styled-components/spinner/spinner.component';
+import { lazy, Suspense } from 'react';
 
-
+const Index = lazy(() => import("./routes/index"));
+const ProfileRoute = lazy(() => import("./routes/profile/profile.route"));
+const Navigation = lazy(() => import("./routes/navigation/navigation"));
+const SignIn = lazy(() => import("./routes/authentication/sign-in"));
+const SignUp = lazy(() => import("./routes/authentication/sign-up"));
+const Folders = lazy(() => import("./routes/folders/folders.route"));
+const NewFolder = lazy(() => import("./components/new-folder/new-folder"));
+const NewButton = lazy(() => import("./components/new-button/new-button"));
+const Donation = lazy(() => import("./routes/donatation/donation"));
 
 function App() {
   return (
-    <div className="App">
-        <Routes>
-          <Route path="/" element={<Navigation />}>
-            <Route index element={<Index />}/>
-            <Route path="sign-in" element={<SignIn />} /> 
-            <Route path="sign-up" element={<SignUp />} /> 
-            <Route path="profile" element={<ProfileRoute />}/>
-            <Route path="folders/*" element={<Folders />}/>
-            <Route path="new-folder" element={<NewFolder />} />
-            <Route path="new-button" element={<NewButton />} />
-            <Route path="donation" element={<Donation />} />
-          </ Route>
-        </Routes>
-    </div>
+    <Suspense fallback={<Spinner />}>
+      <div className="App">
+          <Routes>
+            <Route path="/" element={<Navigation />}>
+              <Route index element={<Index />}/>
+              <Route path="sign-in" element={<SignIn />} /> 
+              <Route path="sign-up" element={<SignUp />} /> 
+              <Route path="profile" element={<ProfileRoute />}/>
+              <Route path="folders/*" element={<Folders />}/>
+              <Route path="new-folder" element={<NewFolder />} />
+              <Route path="new-button" element={<NewButton />} />
+              <Route path="donation" element={<Donation />} />
+            </ Route>
+          </Routes>
+      </div>
+    </Suspense>
   );
 }
 
