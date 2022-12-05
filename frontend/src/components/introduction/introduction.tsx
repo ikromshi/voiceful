be846ab68ad1eb97226a2ff5b773b1d0fe73754d
Ikrom Numonov <numanovikrom454@gmail.com>
2022-12-05 15:31:11 -0500

    issue with app logo resolved

--- frontend/src/components/introduction/introduction.tsx
+++ frontend/src/components/introduction/introduction.tsx
@@ -4,7 +4,7 @@ import "./introduction.css";
 const Introduction = () => {
   const [loopNum] = useState(0);
   const [isDeleting] = useState(false);
-  const wordsToRotate = ["Speak your mind by entering your thoughts into the text box and pressing the “Speak” button for it to be read aloud or use our automatically generated buttons to read our some  or sign in to create your own personalized buttons."];
+  const wordsToRotate = ["Speak your mind by entering your thoughts into the text box and pressing the “Speak” button for it to be read aloud or use our automatically generated buttons to read out text. You can also sign up to create your own personalized buttons."];
   const [text, setText] = useState("");
   const [delta] = useState(20)
 
